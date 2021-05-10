c0f56b10bc653e2140254ab9717313864b22585b
Prayson Wilfred Daniel <prayson.wilfred@itelligence.dk>
2021-05-10 17:46:20 +0200

    gave a better name

--- jobs/dags/subdags/sqlalachemy_pandas_postgres.py
+++ jobs/dags/subdags/sqlalachemy_pandas_postgres.py
@@ -9,83 +9,83 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 
-CONNECTION_URI = BaseHook.get_connection('bolig_db').get_uri()
-TABLE_NAME = 'boliga'
+CONNECTION_URI = BaseHook.get_connection("bolig_db").get_uri()
+TABLE_NAME = "test_boliga"
 
 # time https://airflow.apache.org/docs/1.10.3/_modules/airflow/utils/dates.html
 args = {
-    'owner': 'Prayson',
-    'catchup_by_default': False,
+    "owner": "Prayson",
+    "catchup_by_default": False,
 }
-
 
 
 def load_data(**kwargs):
     """Print the Airflow context and kwargs variable from the context."""
-    
+
     connection = create_engine(CONNECTION_URI)
     # this will come from ds
 
-    df = pd.DataFrame({'house_id': [1, 2, 3],
-                        'price': [11.1, 12.3, 14-5],
-                        'typex': ['A', 'B', 'C'],
-                        'timex': [datetime.now(), datetime.now(), datetime.now()]})
-    df.to_sql(TABLE_NAME,
-                connection,
-                if_exists='append',
-                )
+    df = pd.DataFrame(
+        {
+            "house_id": [1, 2, 3],
+            "price": [11.1, 12.3, 14 - 5],
+            "typex": ["A", "B", "C"],
+            "timex": [datetime.now(), datetime.now(), datetime.now()],
+        }
+    )
+    df.to_sql(
+        TABLE_NAME,
+        connection,
+        if_exists="append",
+    )
 
-    dt = pd.read_sql(f'SELECT * FROM {TABLE_NAME}', connection)
-
+    dt = pd.read_sql(f"SELECT * FROM {TABLE_NAME}", connection)
 
     connection.dispose()
-    print(f'data loaded {len(df)} rows, total rows {len(dt)}')
+    print(f"data loaded {len(df)} rows, total rows {len(dt)}")
 
     return len(dt)
+
 
 def remove_data(**kwargs):
 
     connection = create_engine(CONNECTION_URI)
-    ti = kwargs['ti']
-    data_size = ti.xcom_pull(task_ids='load_data_with_sqlalchemy')
-    
-    print(f'current data: {data_size} rows')
+    ti = kwargs["ti"]
+    data_size = ti.xcom_pull(task_ids="load_data_with_sqlalchemy")
+
+    print(f"current data: {data_size} rows")
     if data_size and data_size > 50:
         with connection.connect() as conn:
             conn.execute(f"DELETE FROM {TABLE_NAME};")
 
-        print(f'data removed {repr(data_size)} rows')
+        print(f"data removed {repr(data_size)} rows")
 
     connection.dispose()
-    return f'[+] data removing task completed'
-    
-
+    return f"[+] data removing task completed"
 
 
 with DAG(
-    dag_id='df_to_postgres_sqlalchemy',
+    dag_id="df_to_postgres_sqlalchemy",
     description=f'Load data to postgress table {repr("boliga")}',
     default_args=args,
-    start_date=datetime.now() - timedelta(minutes=10), # Start 10 minutes ago # days_ago(2)
-    schedule_interval='*/10 * * * *',
-    ) as dag:
+    start_date=datetime.now()
+    - timedelta(minutes=10),  # Start 10 minutes ago # days_ago(2)
+    schedule_interval="*/10 * * * *",
+) as dag:
 
-    
     load_dataframe = PythonOperator(
-        task_id='load_data_with_sqlalchemy',
+        task_id="load_data_with_sqlalchemy",
         python_callable=load_data,
         dag=dag,
-        provide_context=True
-
+        provide_context=True,
     )
 
     remove_dataframe = PythonOperator(
-        task_id='remove_data_after_50_rows',
+        task_id="remove_data_after_50_rows",
         dag=dag,
         python_callable=remove_data,
-        provide_context=True
+        provide_context=True,
     )
 
 
-    
 load_dataframe >> remove_dataframe
