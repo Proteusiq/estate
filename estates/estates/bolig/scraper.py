558ca12df4abd28c182dd763a14c6a43bef9b1ae
Prayson Wilfred Daniel <prayson.wilfred@itelligence.dk>
2022-05-27 17:00:01 +0200

    correct grammar

--- estates/estates/bolig/scraper.py
+++ estates/estates/bolig/scraper.py
@@ -3,9 +3,7 @@ from estates.bolig.core.scrap import Bolig
 
 
 class ScrapEstate:
-    def __init__(
-        self, url: str, api_name: str, scraper_cls: Bolig, params: dict, *args, **kwargs
-    ):
+    def __init__(self, url: str, api_name: str, scraper_cls: Bolig, params: dict, *args, **kwargs):
         self.url = url
         self.api_name = api_name
         self.scraper_cls = scraper_cls
@@ -13,16 +11,14 @@ class ScrapEstate:
 
     def execute(self):
 
-        logger.info(
-            f"\n[+] Using {self.api_name} to demostrate advance web scraping ideas\n"
-        )
+        logger.info(f"\n[+] Using {self.api_name} to demostrate advance web scraping ideas\n")
 
         # instantiate a class
         bolig = self.scraper_cls(url=self.url)
 
         logger.info(
             f'[+] Start {self.params["workers"]} threads for {self.params["pagesize"]} pagesize per call: '
-            f'start at page {self.params["start_page"]} and at page {self.params["end_page"]} \n'
+            f'start at page {self.params["start_page"]} to page {self.params["end_page"]} \n'
         )
         bolig.get_pages(**self.params)
         # homes.DataFrame.drop(columns=['floorPlan', 'pictures'], inplace=True)
